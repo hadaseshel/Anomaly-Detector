fa40bf31fabafdfb73ddf0602e0f426af41a3b74
Hadas Eshel Sofer <hadseshel@gmail.com>
2021-11-08 00:12:40 -0800

    i added all the files

---
+++ SimpleAnomalyDetector.cpp
@@ -0,0 +1,21 @@
+
+#include "SimpleAnomalyDetector.h"
+
+SimpleAnomalyDetector::SimpleAnomalyDetector() {
+
+
+}
+
+SimpleAnomalyDetector::~SimpleAnomalyDetector() {
+
+}
+
+
+void SimpleAnomalyDetector::learnNormal(const TimeSeries& ts){
+
+}
+
+vector<AnomalyReport> SimpleAnomalyDetector::detect(const TimeSeries& ts){
+
+}
+
