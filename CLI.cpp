94253ac721ec67d3e6e0ce502e48bc14441a8a30
Hadas Eshel Sofer <hadseshel@gmail.com>
2021-12-20 00:43:41 -0800

    i added the files of to project

---
+++ CLI.cpp
@@ -0,0 +1,19 @@
+/*
+ * CLI.cpp
+ *
+ * Author: Hadas Eshel 206775074
+ * Partner: Hail Zanbar 322766353
+ */
+
+#include "CLI.h"
+
+CLI::CLI(DefaultIO* dio) {
+}
+
+void CLI::start(){
+}
+
+
+CLI::~CLI() {
+}
+
